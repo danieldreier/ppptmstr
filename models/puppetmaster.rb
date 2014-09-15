d912a1fc6caf07d9326f63baf795763f295888dc
Daniel Dreier <d@puppetlabs.com>
2014-09-14 17:06:25 -0700

    Remove unusable methods from puppetmaster these will need to be reimplemented later

--- models/puppetmaster.rb
+++ models/puppetmaster.rb
@@ -21,38 +21,4 @@ class Puppetmaster < Hashie::Dash
     super
     self.uuid = SecureRandom.uuid unless self.uuid
   end
-
-  def self.list_masters(tenant_id:)
-    # this is a horrible way to do it, I just haven't figured out link walking yet
-    # TODO: rewrite this using link walking to avoid the performance impact of .keys
-
-    client = Riak::Client.new
-    master_bucket="#{tenant_id}-masters"
-
-    masters = []
-    client[master_bucket].keys.each do |key|
-      begin
-        masters << Puppetmaster.new(tenant_id: tenant_id, uuid: key.to_s ).to_hash
-      rescue
-      end
-    end
-    masters
-  end
-
-  def deploy
-    # check if this has been deployed before or is new
-    # load_puppetmaster(self.uuid)
-    # do some kind of validation
-    # submit to queue for creation
-    self.status = 'pending_provisioning'
-#    save_puppetmaster
-  end
-
-  def destroy
-    load_puppetmaster(self.uuid)
-    # do some kind of validation
-    # submit to queue for destruction
-    self.status = 'terminating'
-#    save_puppetmaster
-  end
 end
